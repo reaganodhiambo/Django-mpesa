3ab4608e35c8c4711a160d3d2a972bb7e6f1b0a5
reaganodhiambo <reaganodhiambo003@gmail.com>
2022-08-03 10:43:38 +0300

    Added Serializer

---
+++ daraja/pay/API/serializers.py
@@ -0,0 +1,6 @@
+from rest_framework import  serializers
+from ..models import LNMOnline
+class LNMOnlineSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = LNMOnline
+        fields = '__all__'
